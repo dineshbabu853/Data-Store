813db71719e200da86f73667c2d586d68d1e35e8
Dinesh Babu <dineshbabu853@gmail.com>
2021-01-01 14:31:56 +0530

    update client.js

--- client.js
+++ client.js
@@ -2,13 +2,17 @@
 
 //import the dataStore
 let dataStore = require("./src/dataStore");
+
 //create a object for the data store
+//If you wish to create data store in a specific location provide the file path as argument
+//example : let myDataStore2 = new dataStore("D:\\Project\\data");
+//If file path is not provided then default location will be current working directory
 let myDataStore = new dataStore();
 //get an instance using getInstance method
 let instance = myDataStore.getInstance();
 
 //create operation using create method
-//It take three arguments : key(string) , value(json) , timeToLive(integer) property in seconds - optional
+//It takes three arguments : key(string) , value(json) , timeToLive(integer) property in seconds - optional
 let key = "person1";
 let value = { age: 22, userId: 2345, city: "chennai" };
 let timeToLive = 3;
@@ -17,8 +21,8 @@ instance.create(key, value, timeToLive);
 instance.create("person2", { age: 21, userId: 2346, city: "bangalore" }, 4);
 
 //read operation using get method
-let value = instance.get("person1");
-console.log(value);
+let myVal = instance.get("person1");
+console.log("myVal: ", myVal);
 
 //delete operation using delete method
 instance.delete("person1");
